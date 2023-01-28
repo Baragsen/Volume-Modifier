cc071df18729217acb114bc11f41b79602a0f14d
Baragsen <nkarroud@gmail.com>
2023-01-28 01:24:39 -0800

    Still working on it

--- Volume.c
+++ Volume.c
@@ -1,12 +1,54 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc , char *argv[]){
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
 
-    int volume = argv[3];
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // Copy header from input file to output file
+    uint8_t header_input[HEADER_SIZE];
+    fread(header_input , sizeof(uint8_t) , HEADER_SIZE , input);
+    fwrite(header_input , sizeof(uint8_t) , HEADER_SIZE , output);
+
+    // Read samples from input file and write updated data to output file
+    fseek(input , HEADER_SIZE , SEEK_CUR);
+    fseek(output , HEADER_SIZE , SEEK_CUR );
+
+    while (!feof(input)){
+        int16_t sample;
+        fread(&sample , sizeof(sample), 1 , input);
+        int16_t new_sample = sample * factor;
+        fwrite(&new_sample , sizeof(new_sample) , 1 , output);
+    }
 
 
-
-
-
-
+    // Close files
+    fclose(input);
+    fclose(output);
 }
